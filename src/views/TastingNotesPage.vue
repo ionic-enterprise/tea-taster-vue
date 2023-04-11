358fdd6813be4dabe7b09b2afa92f47908181cad
Ken Sodemann <ken@ionic.io>
2023-04-11 17:17:45 -0500

    step(thirteen): add the notes feature

--- src/views/TastingNotesPage.vue
+++ src/views/TastingNotesPage.vue
@@ -1,17 +1,81 @@
 <template>
-  <ion-page>
-    <ion-header>
+  <ion-page ref="page">
+    <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Tasting Notes</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content></ion-content>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tasting Notes</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-list data-testid="notes-list">
+        <ion-item-sliding v-for="note of notes" :key="note.id">
+          <ion-item button @click="presentNoteEditor($event, note.id)">
+            <ion-label>
+              <div>{{ note.brand }}</div>
+              <div>{{ note.name }}</div>
+            </ion-label>
+          </ion-item>
+
+          <ion-item-options>
+            <ion-item-option color="danger" @click="remove(note)"> Delete </ion-item-option>
+          </ion-item-options>
+        </ion-item-sliding>
+      </ion-list>
+
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button @click="presentNoteEditor" data-testid="add-note-button">
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  modalController,
+} from '@ionic/vue';
+import { add } from 'ionicons/icons';
+import AppTastingNoteEditor from '@/components/AppTastingNoteEditor.vue';
+import { useTastingNotes } from '@/composables/tasting-notes';
+import { ref } from 'vue';
+
+const page = ref(null);
+const { notes, refresh, remove } = useTastingNotes();
+
+const presentNoteEditor = async (evt: Event, noteId?: number) => {
+  const modal = await modalController.create({
+    component: AppTastingNoteEditor,
+    presentingElement: (page.value as any).$el,
+    backdropDismiss: false,
+    componentProps: {
+      noteId,
+    },
+  });
+  modal.present();
+};
+
+refresh();
 </script>
 
 <style scoped></style>
--- src/views/__tests__/TastingNotesPage.spec.ts
+++ src/views/__tests__/TastingNotesPage.spec.ts
@@ -1,8 +1,12 @@
+import { useTastingNotes } from '@/composables/tasting-notes';
 import TastingNotesPage from '@/views/TastingNotesPage.vue';
-import { IonTitle } from '@ionic/vue';
+import { IonTitle, modalController } from '@ionic/vue';
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { mount, VueWrapper } from '@vue/test-utils';
+import { vi } from 'vitest';
 import { Router } from 'vue-router';
+
+vi.mock('@/composables/tasting-notes');
 
 describe('TastingNotesPage.vue', () => {
   let router: Router;
@@ -21,10 +25,78 @@ describe('TastingNotesPage.vue', () => {
     });
   };
 
+  beforeEach(() => {
+    const { notes } = useTastingNotes();
+    notes.value = [
+      {
+        id: 42,
+        brand: 'Lipton',
+        name: 'Green Tea',
+        teaCategoryId: 3,
+        rating: 3,
+        notes: 'A basic green tea, very passable but nothing special',
+      },
+      {
+        id: 314159,
+        brand: 'Lipton',
+        name: 'Yellow Label',
+        teaCategoryId: 2,
+        rating: 1,
+        notes: 'Very acidic, even as dark teas go, OK for iced tea, horrible for any other application',
+      },
+      {
+        id: 73,
+        brand: 'Rishi',
+        name: 'Puer Cake',
+        teaCategoryId: 6,
+        rating: 5,
+        notes: 'Smooth and peaty, the king of puer teas',
+      },
+    ];
+    vi.resetAllMocks();
+  });
+
   it('displays the title', async () => {
     const wrapper = await mountView();
     const titles = wrapper.findAllComponents(IonTitle);
-    expect(titles).toHaveLength(1);
+    expect(titles).toHaveLength(2);
     expect(titles[0].text()).toBe('Tasting Notes');
+    expect(titles[1].text()).toBe('Tasting Notes');
+  });
+
+  it('refreshes the tasting notes data', async () => {
+    const { refresh } = useTastingNotes();
+    await mountView();
+    expect(refresh).toHaveBeenCalledTimes(1);
+  });
+
+  it('displays the notes', async () => {
+    const wrapper = await mountView();
+    const list = wrapper.find('[data-testid="notes-list"]');
+    const items = list.findAll('ion-item');
+    expect(items.length).toBe(3);
+    expect(items[0].text()).toContain('Lipton');
+    expect(items[0].text()).toContain('Green Tea');
+    expect(items[1].text()).toContain('Lipton');
+    expect(items[1].text()).toContain('Yellow Label');
+    expect(items[2].text()).toContain('Rishi');
+    expect(items[2].text()).toContain('Puer Cake');
+  });
+
+  describe('adding a new note', () => {
+    let modal: { present: () => Promise<void> };
+    beforeEach(() => {
+      modal = {
+        present: vi.fn().mockResolvedValue(undefined),
+      };
+      modalController.create = vi.fn().mockResolvedValue(modal);
+    });
+
+    it('displays the modal', async () => {
+      const wrapper = await mountView();
+      const button = wrapper.findComponent('[data-testid="add-note-button"]');
+      await button.trigger('click');
+      expect(modal.present).toHaveBeenCalledTimes(1);
+    });
   });
 });
